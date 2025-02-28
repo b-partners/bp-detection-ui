78df500be817eac82007b356591d706d90bca810
amourRamanantsiresy <hei.amour.2@gmail.com>
2025-02-28 10:42:41 +0300

    chore: remove unused code & update ref

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import '@fontsource/roboto/700.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
