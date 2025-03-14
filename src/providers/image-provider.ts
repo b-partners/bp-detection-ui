c3626b9c68b7abd0a17f5e22cba751bec29955b1
amourRamanantsiresy <hei.amour.2@gmail.com>
2025-03-14 10:13:12 +0300

    chore: implement get image from address provider

--- src/providers/image-provider.ts
+++ src/providers/image-provider.ts
@@ -1,5 +1,16 @@
-export const getImageFromAddress = async () => {
-  const result = await fetch(`${process.env.API_URL}/image`);
-  const { imageUrl } = await result.json();
-  return imageUrl;
+import { ZoomLevel } from '@bpartners/typescript-client';
+import { v4 } from 'uuid';
+import { bpAnnotationApi } from './api';
+import { userInfoProvider } from './user-info-provider';
+
+export const getImageFromAddress = async (apiKey: string, address: string) => {
+  const { accountId } = await userInfoProvider(apiKey);
+  const { data: areaPictureDetails } = await bpAnnotationApi(apiKey).crupdateAreaPictureDetails(accountId ?? '', v4(), {
+    address,
+    fileId: v4(),
+    filename: `Layer ${address}`,
+    zoomLevel: ZoomLevel.HOUSES_0,
+  });
+
+  return areaPictureDetails;
 };
--- src/providers/user-info-provider.ts
+++ src/providers/user-info-provider.ts
@@ -7,21 +7,17 @@ const getCurrentAccount = (accounts: Account[]) => {
 };
 
 export const userInfoProvider = async (apiKey: string) => {
-  try {
-    const userInfo = { ...getCached.userInfo() };
-    if (!userInfo.userId) {
-      const { data: whoami } = await bpSecurityApi(apiKey).whoami();
-      const { user } = whoami;
-      userInfo.userId = user?.id ?? '';
-    }
-    if (!userInfo.accountId) {
-      const { data: accounts } = await bpUserAccountApi(apiKey).getAccountsByUserId(userInfo.userId ?? '');
-      const currentAccount = getCurrentAccount(accounts);
-      userInfo.accountId = currentAccount?.id ?? '';
-    }
-    cache.userInfo(userInfo.userId ?? '', userInfo.accountId ?? '');
-    return { ...userInfo };
-  } catch (err) {
-    console.log(err);
+  const userInfo = { ...getCached.userInfo() };
+  if (!userInfo.userId) {
+    const { data: whoami } = await bpSecurityApi(apiKey).whoami();
+    const { user } = whoami;
+    userInfo.userId = user?.id ?? '';
   }
+  if (!userInfo.accountId) {
+    const { data: accounts } = await bpUserAccountApi(apiKey).getAccountsByUserId(userInfo.userId ?? '');
+    const currentAccount = getCurrentAccount(accounts);
+    userInfo.accountId = currentAccount?.id ?? '';
+  }
+  cache.userInfo(userInfo.userId ?? '', userInfo.accountId ?? '');
+  return { ...userInfo };
 };
