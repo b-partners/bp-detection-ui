025c949169de90ea46f96f88fc25bd3126714a81
amourRamanantsiresy <hei.amour.2@gmail.com>
2025-03-14 10:24:54 +0300

    chore: update query image impl

--- src/queries/image-queries.ts
+++ src/queries/image-queries.ts
@@ -1,7 +1,13 @@
+import { getQueryParams } from '@/utilities';
 import { useMutation } from '@tanstack/react-query';
 import { getImageFromAddress } from '../providers';
 
+const mutationFn = (address: string) => {
+  const { apiKey } = getQueryParams();
+  return getImageFromAddress(apiKey, address);
+};
+
 export const useQueryImageFromAddress = () => {
-  const { isPending, data, mutate } = useMutation({ mutationKey: ['image from address'], mutationFn: getImageFromAddress });
+  const { isPending, data, mutate } = useMutation({ mutationKey: ['image from address'], mutationFn });
   return { isQueryImagePending: isPending, imageSrc: data, queryImage: mutate };
 };
