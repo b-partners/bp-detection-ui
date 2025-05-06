68ceae6c55b205b7839d2c6de9c8202a0de02317
kirioka <hei.amour.2@gmail.com>
2025-05-06 17:37:19 +0300

    fix: use vggUrl for detection result

--- src/mappers/index.ts
+++ src/mappers/index.ts
@@ -1,5 +1,6 @@
 export * from './annotator-mapper';
 export * from './constants';
+export * from './detection-result-mapper';
 export * from './geojson-mapper';
 export * from './polygon-mapper';
 export * from './types';
