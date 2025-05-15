04336a478bdbe1300263af729953e524a276a7c9
kirioka <hei.amour.2@gmail.com>
2025-05-15 15:33:37 +0300

    test: e2e detection on extended image

--- cypress/e2e/detection.cy.ts
+++ cypress/e2e/detection.cy.ts
@@ -3,8 +3,8 @@ const canvas_cursor_sel = 'annotator-canvas-cursor';
 const process_detection_sel = 'process-detection-button';
 const process_detection_on_form_sel = 'process-detection-on-form-button';
 
-describe('template spec', () => {
-  it('passes', () => {
+describe('test detection', () => {
+  beforeEach(() => {
     cy.intercept('POST', '/address/autocomplete*').as('location-search');
 
     // user informations
@@ -30,7 +30,9 @@ describe('template spec', () => {
     cy.intercept('POST', '/Prod/mercator').as('mercatorConversion');
 
     cy.visit('https://roof.birdia.fr');
+  });
 
+  it('1024 image detection', () => {
     //steppers state
     cy.contains('Récupération de votre adresse').should('have.class', 'Mui-active');
     cy.contains('Délimitation de votre toiture').should('not.have.class', 'Mui-active');
@@ -93,6 +95,78 @@ describe('template spec', () => {
 
     cy.contains('220.77m²');
     cy.contains("Taux d'humidité").parent('.MuiStack-root').siblings('.MuiTypography-root').contains('0.3%');
+    cy.contains('Obstacle / Velux').parent('.MuiStack-root').siblings('.MuiTypography-root').contains('OUI');
+  });
+
+  it('extended image detection', () => {
+    //steppers state
+    cy.contains('Récupération de votre adresse').should('have.class', 'Mui-active');
+    cy.contains('Délimitation de votre toiture').should('not.have.class', 'Mui-active');
+    //steppers state
+
+    cy.contains("Clé d'API invalide");
+    cy.contains("Votre clé d'API est invalide. Veuillez specifier une clé valide");
+
+    cy.dataCy('api-key-input').type(process.env.REACT_PROD_API_KEY || '');
+    cy.contains('Valider').click();
+
+    cy.contains('Récupération de votre adresse');
+    cy.dataCy(search_input_sel).type('12 Boulevard de la Croisette, 06400 Cannes');
+    cy.wait('@location-search');
+
+    cy.contains('12 Boulevard de la Croisette, 06400 Cannes, France').click();
+
+    cy.wait('@getWhoami');
+    cy.wait('@getAccounts');
+    cy.wait('@getAccountHolders');
+    cy.wait('@createProspect');
+    cy.wait('@createAreaPicture');
+    cy.wait('@getImage');
+    cy.wait('@createDetection');
+
+    cy.contains("Veuillez délimiter votre toiture sur l'image suivante.");
+
+    cy.get('button').contains(`Recentrer`).click();
+
+    cy.dataCy('zoom-in').click();
+    cy.dataCy('zoom-in').click();
+    cy.dataCy('zoom-reset').click();
+
+    const getX = (x: number) => Math.floor(x + 70);
+    const getY = (y: number) => Math.floor(y + 4);
+
+    cy.dataCy(canvas_cursor_sel).click(getX(244), getY(175), { force: true });
+    cy.dataCy(canvas_cursor_sel).click(getX(225), getY(244), { force: true });
+    cy.dataCy(canvas_cursor_sel).click(getX(232), getY(256), { force: true });
+    cy.dataCy(canvas_cursor_sel).click(getX(305), getY(276), { force: true });
+    cy.dataCy(canvas_cursor_sel).click(getX(334), getY(215), { force: true });
+    cy.dataCy(canvas_cursor_sel).click(getX(336), getY(202), { force: true });
+    cy.dataCy(canvas_cursor_sel).click(getX(244), getY(175), { force: true });
+
+    cy.dataCy(process_detection_sel).should('not.have.class', 'Mui-disabled');
+
+    cy.dataCy(process_detection_sel).click();
+
+    cy.contains('Veuillez saisir les informations suivantes.');
+
+    cy.dataName('lastName').type('Doe');
+    cy.dataName('firstName').type('John');
+    cy.dataName('phone').type(process.env.REACT_IT_TEST_PHONE || '');
+    cy.dataName('email').type(process.env.REACT_IT_TEST_EMAIL || '');
+
+    cy.dataCy(process_detection_on_form_sel).click();
+
+    cy.wait('@createDetectionImage');
+    cy.wait('@mercatorConversion');
+    cy.wait('@createProspect');
+    cy.wait('@createDetection');
+    cy.wait('@getDetection');
+
+    cy.contains('210.86m²');
+    cy.contains('Note de dégradation globale : 3.74%');
+    cy.contains("Taux d'usure").parent('.MuiStack-root').siblings('.MuiTypography-root').contains('0.77%');
+    cy.contains('Taux de moisissure').parent('.MuiStack-root').siblings('.MuiTypography-root').contains('11.46%');
+    cy.contains("Taux d'humidité").parent('.MuiStack-root').siblings('.MuiTypography-root').contains('0%');
     cy.contains('Obstacle / Velux').parent('.MuiStack-root').siblings('.MuiTypography-root').contains('OUI');
   });
 });
