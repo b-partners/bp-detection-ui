bb8a7236d292e5980adf65fe5a7517f80bf39806
kirioka <hei.amour.2@gmail.com>
2025-09-11 20:52:45 +0300

    fix: annotator section top bar

--- src/components/annotator-canvas-custom.tsx
+++ src/components/annotator-canvas-custom.tsx
@@ -1,9 +1,7 @@
-import { useStep } from '@/hooks';
 import { AnnotatorCanvas, AnnotatorCanvasProps } from '@bpartners/annotator-component';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { FC, ReactNode } from 'react';
 import { AnnotatorCustomButton } from './annotator-custom-button';
-import { addressStyle } from './style';
 
 interface AnnotatorCanvasCustomProps extends Omit<AnnotatorCanvasProps, 'buttonsComponent' | 'width' | 'height' | 'zoom'> {
   isLoading?: boolean;
@@ -12,14 +10,8 @@ interface AnnotatorCanvasCustomProps extends Omit<AnnotatorCanvasProps, 'buttons
 }
 
 export const AnnotatorCanvasCustom: FC<AnnotatorCanvasCustomProps> = ({ isLoading, customButtons, height, ...props }) => {
-  const {
-    params: { areaPictureDetails },
-  } = useStep();
   return (
     <>
-      <Box sx={addressStyle}>
-        <Typography>Adresse: {areaPictureDetails?.address}</Typography>
-      </Box>
       <Box position='relative' sx={{ minHeight: '500px' }}>
         {(isLoading || props.image.length === 0) && (
           <Box width='100%' height='600px' display='flex' justifyContent='center' alignItems='center' bgcolor={theme => theme.palette.grey[100]}></Box>
