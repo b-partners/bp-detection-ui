165ccb2b003c14b9a1016c30ccc6cdc178d08abf
kirioka <hei.amour.2@gmail.com>
2025-09-21 21:14:50 +0300

    chore: llm result do not need slope

--- src/queries/llm-result-queries.ts
+++ src/queries/llm-result-queries.ts
@@ -21,5 +21,5 @@ export const useLlmResultQuery = (roofAnnotatorProperties: Properties & { obstac
     return htmlResult;
   };
 
-  return useQuery({ queryFn, queryKey: [roofAnnotatorProperties], enabled: !!pente });
+  return useQuery({ queryFn, queryKey: [roofAnnotatorProperties] });
 };
