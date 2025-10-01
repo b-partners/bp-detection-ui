723cefc3718b2615cb04dda65e3e261aa675d176
kirioka <hei.amour.2@gmail.com>
2025-10-01 20:36:58 +0300

    fix: llm style & remove <head /> inside <body />

--- src/queries/llm-result-queries.ts
+++ src/queries/llm-result-queries.ts
@@ -7,7 +7,8 @@ const baseUrl = `${process.env.LLM_ANALYSE_RESULT}`;
 const apiKey = `${process.env.LLM_API_KEY}`;
 
 export const useLlmResultQuery = (roofAnnotatorProperties: Properties & { obstacle: boolean }) => {
-  const { moisissure_rate, usure_rate, humidite_rate, roof_area_in_m2, revetement_1, global_rate_value, obstacle, global_rate_type } = roofAnnotatorProperties || {};
+  const { moisissure_rate, usure_rate, humidite_rate, roof_area_in_m2, revetement_1, global_rate_value, obstacle, global_rate_type } =
+    roofAnnotatorProperties || {};
   const queryFn = async () => {
     let llmResult = getCached.llmResult();
     if (llmResult) return llmResult;
@@ -16,7 +17,8 @@ export const useLlmResultQuery = (roofAnnotatorProperties: Properties & { obstac
       `${baseUrl}?surfaceEnM2=${roof_area_in_m2}&revetement=${fromAnalyseResultToDomain(revetement_1)}&moisissure=${moisissure_rate}&usure=${usure_rate}&obstacles=${JSON.stringify(obstacle)}&risqueFeu=false&fissureCassure=false&noteDegradationGlobale=${global_rate_value}&category=${global_rate_type}&humidit%C3%A9=${humidite_rate}&x-api-key=${apiKey}`
     );
 
-    const htmlResult = await result.text();
+    const _htmlResult = await result.text();
+    const htmlResult = _htmlResult.split('</head>')[1];
     cache.llmResult(htmlResult || '');
     return htmlResult;
   };
