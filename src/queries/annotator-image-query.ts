d1ba9c5ec324f55a30a3c88576b6b81901035014
amourRamanantsiresy <hei.amour.2@gmail.com>
2026-01-19 10:38:10 +0300

    style: format code

--- src/queries/annotator-image-query.ts
+++ src/queries/annotator-image-query.ts
@@ -1,14 +1,14 @@
+import { useStep } from '@/hooks';
 import { fileProvider } from '@/providers/file-provider';
-import { useMutation } from '@tanstack/react-query';
+import { cache, getCached } from '@/utilities';
 import { base64ToFile } from '@/utilities/file-utilities';
 import { FileType } from '@bpartners/typescript-client';
-import { cache, getCached } from '@/utilities';
-import { useStep } from '@/hooks';
+import { useMutation } from '@tanstack/react-query';
 
 const mutationFn = (fileId: string) => async (file: string) => {
   if (getCached.isAnalyzeImageAlreadyUploaded()) return;
   const arrayBuffer = base64ToFile(file, fileId + '.png');
-  const result = [await fileProvider.upload(fileId, FileType.AREA_PICTURE, arrayBuffer)]
+  const result = [await fileProvider.upload(fileId, FileType.AREA_PICTURE, arrayBuffer)];
   cache.isAnalyzeImageAlreadyUploaded();
   return result;
 };
@@ -20,6 +20,6 @@ interface Params {
 
 export const useAnnotatorImageUploadQuery = (params?: Params) => {
   const { onError, onSuccess } = params || {};
-  const {  areaPictureDetails } = useStep(({ params }) => params);
+  const { areaPictureDetails } = useStep(({ params }) => params);
   return useMutation({ mutationFn: mutationFn(areaPictureDetails?.fileId || ''), onSuccess, onError });
 };
